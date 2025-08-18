d0bd749c79f2d76955bb290d385ad2a676c2d086
kevinjamesus86 <kevinjamesus86@users.noreply.github.com>
2025-08-18 16:12:47 +0000

    Update macro to 0.1.6

--- Formula/macro.rb
+++ Formula/macro.rb
@@ -1,28 +1,28 @@
 class Macro < Formula
   desc "Macro Terminal"
   homepage "https://github.com/getmacro/terminal"
-  version "0.1.5"
+  version "0.1.6"
   license "MIT"
 
   depends_on "unixodbc" => :optional
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/getmacro/terminal/releases/download/v0.1.5/macro-aarch64-apple-darwin.tar.gz"
-      sha256 "a62100a29722f9170580c7adf7404b961c4f7e59a5c5940b91e95fb0e0504b99"
+      url "https://github.com/getmacro/terminal/releases/download/v0.1.6/macro-aarch64-apple-darwin.tar.gz"
+      sha256 "ecfde3353ce290106dbaa68e246008c76c6f0d512069ed6313ab0d34d7dbcfc6"
     else
-      url "https://github.com/getmacro/terminal/releases/download/v0.1.5/macro-x86_64-apple-darwin.tar.gz"
-      sha256 "e06053eb3b81fc08fe34b8fa598f87eb9cf03da9f43956711812650c08e9fd53"
+      url "https://github.com/getmacro/terminal/releases/download/v0.1.6/macro-x86_64-apple-darwin.tar.gz"
+      sha256 "d2b68366d20a6dcdd243c4067df3f2aac914c57ee4320a7650665473b4e7dc88"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/getmacro/terminal/releases/download/v0.1.5/macro-aarch64-linux-gnu.tar.gz"
-      sha256 "f7e89648cb71ccbc279d1230a8ac1feaddc1b5ac39d3cb33ab8135abdbcc3014"
+      url "https://github.com/getmacro/terminal/releases/download/v0.1.6/macro-aarch64-linux-gnu.tar.gz"
+      sha256 "0150933dd3a5c9e267e24eac815628e47e016d154c0face566c49c076cdd0ed6"
     else
-      url "https://github.com/getmacro/terminal/releases/download/v0.1.5/macro-x86_64-linux-gnu.tar.gz"
-      sha256 "b8a88418e0b3a0d2e8a5be3b8ad75fe26ed2ef100978ff96938be95a91bca981"
+      url "https://github.com/getmacro/terminal/releases/download/v0.1.6/macro-x86_64-linux-gnu.tar.gz"
+      sha256 "694c63912b652c42bb0ac31899bbc4e1cb8d1de7eb45f1d478be7b79fa48c2d4"
     end
   end
 
